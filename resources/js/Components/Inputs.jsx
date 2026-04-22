5e1a4a776c957f9b74955b20ef7a86948b445ab7
Syariffullah <dinnereatc@gmail.com>
2026-04-22 19:21:28 +0700

    refactor: implement reusable input components for login page

---
+++ resources/js/Components/Inputs.jsx
@@ -0,0 +1,89 @@
+import { Icon } from "@iconify/react";
+import { useState } from "react";
+
+export function TextInput({
+    htmlFor,
+    id,
+    name,
+    placeholder,
+    value,
+    onChange,
+    text,
+}) {
+    return (
+        <div className="flex flex-col gap-2">
+            <label htmlFor={htmlFor} className="text-base font-medium">
+                {text}
+            </label>
+            <input
+                id={id}
+                name={name}
+                placeholder={placeholder}
+                type="text"
+                value={value}
+                onChange={onChange}
+                autoFocus
+                required
+                className="border border-default/30 placeholder:text-neutral font-body p-3 text-sm w-full focus:border-blue-500 focus:outline-blue-500 rounded-lg"
+            />
+        </div>
+    );
+}
+
+export function PasswordInput({
+    htmlFor,
+    id,
+    name,
+    value,
+    placeholder,
+    onClick,
+    onChange,
+    text,
+    errors,
+}) {
+    const [showPassword, setShowPassword] = useState(false);
+    return (
+        <div className="flex flex-col gap-2">
+            <label htmlFor={htmlFor} className="text-base font-medium">
+                {text}
+            </label>
+            <div className="relative flex items-center justify-between">
+                <input
+                    id={id}
+                    name={name}
+                    placeholder={placeholder}
+                    type={showPassword ? "text" : "password"}
+                    value={value}
+                    onChange={onChange}
+                    required
+                    className="border border-default/30 placeholder:text-neutral font-body p-3 text-sm w-full focus:border-blue-500 focus:outline-blue-500 rounded-lg"
+                />
+
+                <button
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="absolute right-3 top-1/2 -translate-y-1/2 text-neutral cursor-pointer"
+                    tabIndex={-1}
+                >
+                    {showPassword ? (
+                        <Icon icon="iconoir:eye-closed" className="w-5 h-5" />
+                    ) : (
+                        <Icon icon="iconoir:eye-solid" className="w-5 h-5" />
+                    )}
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export function Checkbox({ name, checked, onChange }) {
+    return (
+        <input
+            type="checkbox"
+            name={name}
+            checked={checked}
+            onChange={onChange}
+            className="h-4 w-4 rounded checked:bg-blue-500 checked:border-blue-500 focus:ring-blue-500 cursor-pointer"
+        />
+    );
+}
